12ab74d27f4dfde243bd9b8589a5f3f75fb53763
alexue <xueyx1619@gamil.com>
2021-02-13 01:38:30 +0800

    test5

--- Shiny Application and Reproducible Pitch/project/server.R
+++ Shiny Application and Reproducible Pitch/project/server.R
@@ -5,7 +5,7 @@ library(datasets)
 
 server <- function(input, output) {
   output$barplot <- renderPlot({
-    color <- c("green", "yellow")
+    color <- c("lightblue", "yellow")
     barplot(WorldPhones[, input$region],
             main = input$region,
             xlab = "Year",
